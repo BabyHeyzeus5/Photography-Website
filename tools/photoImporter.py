2bdc4eed22659ef72c1ec3de6b3e5d5188e25044
BabyHeyzeus5 <andrewjjesus2@gmail.com>
2024-07-15 18:45:29 -0500

    new file:   landingPage.html 	new file:   landingStyle.css 	new file:   pages/albums/spring break - Copy/IMG_6163.JPG 	new file:   pages/albums/spring break - Copy/IMG_6174.JPG 	new file:   pages/albums/spring break - Copy/IMG_6193.JPG 	new file:   pages/albums/spring break - Copy/IMG_6196.JPG 	new file:   pages/albums/spring break - Copy/IMG_6199.JPG 	new file:   pages/albums/spring break - Copy/IMG_6200.JPG

---
+++ tools/photoImporter.py
@@ -0,0 +1,48 @@
+import os
+from PIL import Image
+
+#----------EDIT THESE-----------------#
+targetFolder = "C:\\Users\\nebra\\OneDrive\\Desktop\\Coding Projects\\Photography Website\\pages\\albums\\spring break"
+targetFile = "C:\\Users\\nebra\\OneDrive\\Desktop\\Coding Projects\\Photography Website\\pages\\springBreak.html"
+albumName = "spring break"
+
+
+def compressImages(folder):
+    photos = os.listdir(folder)
+    i = 0
+    for photo in photos:
+        filePath = os.path.join(targetFolder, photo)
+        picture = Image.open(filePath)
+        picture.save(filePath,"JPEG", optimize = True, subsamppling = 0, quality = 50)
+        i += 1
+        print(f"finished {i} photos")
+    return
+
+def generateHTML(folder):
+    photos = os.listdir(folder)
+    html_string = "\t\t\t<a target=\"_blank\" href=\".\\albums\\{}\\{}\"><img src=\".\\albums\\{}\\{}\" alt=\"Photo in Album\"></a>\n"
+    messageList = []
+    for photo in photos:
+        messageList.append(html_string.format(albumName, photo, albumName, photo))
+    return messageList
+
+def appendFile(file, htmls):
+    #begin going through the target file until the START condition is found
+    fileString = []
+    with open(targetFile, newline='') as file:
+        for line in file:
+            fileString.append(line)
+            if line.strip() == "<!--START-->":
+                #begin adding the photos in
+                for html in htmls:
+                    fileString.append(html)
+        file.close()
+        file2 = open(targetFile, 'w', newline='')
+        for line in fileString:
+            file2.write(line)
+        file2.close()
+        print(f"Finished. Added {len(htmls)} photos.")
+
+compressImages(targetFolder)
+htmls = generateHTML(targetFolder)
+appendFile(targetFile, htmls)
